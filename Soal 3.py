ee8fb5de8542f7181b4b51ab1ef8c3eb3ab3c1b0
winter-256 <zakimuhammad275@gmail.com>
2026-08-25 22:26:03 +0800

    second commit

--- Soal 3.py
+++ Soal 3.py
@@ -1,13 +1,10 @@
 print("=== PROGRAM KASIR KANTIN ===") 
 
-# Mengambil input dari pengguna
 harga = int(input("Masukkan harga makanan: ")) 
 jumlah = int(input("Masukkan jumlah porsi: ")) 
 
-# Menghitung total awal
 total_bayar = harga * jumlah 
 
-# Cek kondisi diskon
 if total_bayar > 20000: 
     potongan = total_bayar * 0.1  # Diskon 10%
     total_bayar = total_bayar - potongan
@@ -15,5 +12,4 @@ if total_bayar > 20000:
 else: 
     print("Maaf, belum mencapai batas minimal diskon") 
 
-# Menampilkan total akhir
 print(f"Total yang harus dibayar: Rp{int(total_bayar)}")
